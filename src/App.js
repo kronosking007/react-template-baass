a6b3940d44faad489595ef31d13a0ce975ac1a34
AdityaNathK <aditya.nath@aligent.com.au>
2022-02-23 10:50:36 +1030

    Add scss structure as per pwa

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>React Template</h1>
+      <Button />
     </div>
   );
 }
--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,5 +1,4 @@
 // This file contains all the jsx code related to the component
-
 import React from "react";
 import "./Button.scss";
 
